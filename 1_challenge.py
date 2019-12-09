e1a4c01c12ebea4b1e80ee60af7bdcf483451200
DeplorableJed <48975520+DeplorableJed@users.noreply.github.com>
2019-12-09 01:47:40 -0800

    Update 1_challenge.py

--- 1_challenge.py
+++ 1_challenge.py
@@ -4,18 +4,18 @@
 # print the message
 # There was a single quote inside the string!
 # Use double quotes to enclose the string
-print("Why won't this line of code print")
+print('Why won't this line of code print')
 
 # print the message
 # There was a mistake in the function name
-print('This line fails too!')
+prnt('This line fails too!')
 
 # print the message
 # Need to add the () around the string
-print ("I think I know how to fix this one")
+print "I think I know how to fix this one"
 
 # print the name entered by the user
 # You need to store the value returned by the input statement
 # in a variable
-name = input('Please tell me your name: ')
-print(name)
+input('Please tell me your name: ')
+print(input)
