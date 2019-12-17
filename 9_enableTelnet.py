e08aac2350d9dc797e992055be98032ca0c6cfe3
DeplorableJed <48975520+DeplorableJed@users.noreply.github.com>
2019-12-17 13:12:48 -0800

    Create 9_enableTelnet.py

---
+++ 9_enableTelnet.py
@@ -0,0 +1,24 @@
+import paramiko
+import time
+
+routers = routers = ["10.3.0.1", "10.3.1.2", "10.3.3.3"]
+username = "Admin"
+password = "NterOne1!"
+
+sshcon = paramiko.SSHClient()
+sshcon.setmissinghostkeypolicy(paramiko.AutoAddPolicy())
+for target in routers:
+    sshcon.connect(hostname=target,username=username,password=password)
+    print ("Successful connection", target)
+    remoteconnection = sshcon.invokeshell()
+    remote_connection.send("configure terminal\n")
+    remote_connection.send("line vty 0 4\n")
+    remote_connection.send("transport input all\n")
+    remote_connection.send("exit\n")
+    remote_connection.send("exit\n")
+    remote_connection.send("copy run start\n")
+    time.sleep(1)
+    output = remote_connection.recv(65535)
+    print (output)
+    sshcon.close
+print("Job completed succesfully")
